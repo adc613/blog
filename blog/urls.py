66a263cebcf1acc5344011a7059db5f50e62290c
Adam Collins <adc82@case.edu>
2015-04-03 20:38:32 -0400

    inital commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from entries.views import home
+
+urlpatterns = [
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', home  , name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+]
