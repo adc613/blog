6643aa68c0327927b2b304fc9585e8497a96bf7c
Adam Collins <adc82@case.edu>
2015-07-30 09:17:47 -0400

    nginx serving media files

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,3 @@ urlpatterns = [
     url(r'^entries/', include('entries.urls', namespace='entries')),
 ]
 
-from django.conf.urls.static import  static
-from blog import settings
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
