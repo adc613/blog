54749f85457094b30831c6186ead237f7f0e1ee0
Adam Collins <adc82@case.edu>
2015-07-30 00:10:41 -0400

    got media files working

--- blog/urls.py
+++ blog/urls.py
@@ -9,3 +9,7 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^entries/', include('entries.urls', namespace='entries')),
 ]
+
+from django.conf.urls.static import  static
+from blog import settings
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
