17ac85cfa2443980649c357318c231b449eec647
Adam Collins <adc82@case.edu>
2015-08-13 17:10:29 -0400

    stash

--- debate/models.py
+++ debate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Debate(models.Model):
+    title = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Argument(models.Model):
+    argument = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey('accounts.User')
+    dabte_thread = models.ForeignKey(Debate)
+
+    def __str__(self):
+        return self.debate.title + ': ' + self.creation_date + ', ' + str(self.creator)
--- debate/views.py
+++ debate/views.py
@@ -1,3 +1,20 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import View
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Argument
+from .models import Debate
 
 # Create your views here.
+class DebateListView(View):
+    template_name = 'debate/list.html'
+
+    def get(self, request, **kwargs):
+        context = {}
+        debate = Debate.objects.get_or_404(pk=kwargs['pk'])
+        context['debate'] = debate
+        context['arguments'] = Argument.objects.filter(
+                                 debate__title=debate.title)
