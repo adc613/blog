c10b05cde9e870c3e6ce24c7cefe5c74ec5d6191
Adam Collins <adc82@case.edu>
2015-12-21 16:21:52 -0600

    angular working...

--- static-only/angular/entries/app.js
+++ static-only/angular/entries/app.js
@@ -1,84 +1,129 @@
 (function(){
-    var app = angular.module('blogApp', ['ngRoute']);
-    
-    app.config(function($interpolateProvider, $routeProvider) {
-        $interpolateProvider.startSymbol('[[');
-        $interpolateProvider.endSymbol(']]');
-        $routeProvider
-            .when('/entries/:entryID', {
-                templateUrl: '/static/angular/detail.html',
-                controller: 'DetailController'
-            })
-            .otherwise({
-                redirectTo: '/',
-                templateUrl: '/static/angular/list.html',
-                controller: 'BlogController'
-            });
-    });
-    
-    app.factory('Data', ['$http', function($http) {
-        var data = [];
-        var promise;
-        
-        var req =  {
-            method: 'GET',
-            url: '/entries/api/entries/?format=json',
-        };
-        
-        return {
-            getData: function(){
-                if(!promise){
-                    promise = $http(req).then(
-                        function(resp){
-                            data = resp.data;
-                            return data;
-                        }, 
-                        function(resp){
-                            return resp;
-                        });
-                };
-                return promise;
-                },
-            getDetail: function(id) {
-                return data[id];
-            }
-        };
-    }]);
-    
-    app.controller('BlogController', ['$http','$scope', 'Data', function($http, $scope, Data){
-        var a = true;
-        Data.getData().then(
-            function(data){
-                $scope.articles = data;
-            },
-            function(resp){
-                console.log('error....');
-                console.log(resp);
-            });
-        a = false;
-    }]);
-    
+  var app = angular.module('blogApp', ['ngRoute', 'ngSanitize']);
+  
 
-    app.directive('blogEntry', function() {
-        return {
-            restrict: 'E',
-            templateUrl: '/static/angular/entry.html',
-            controller: 'BlogController',
-            controllerAs: 'BlogCtrl'
+    
+  app.config(function($interpolateProvider, $routeProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+    $routeProvider
+      .when('/entries/:entryID', {
+        templateUrl: '/static/angular/detail.html',
+        controller: 'DetailController'
+      })
+      .otherwise({
+        redirectTo: '/',
+        templateUrl: '/static/angular/list.html',
+        controller: 'BlogController'
+      });
+  });
+  
+  app.filter('articleFormatter', function(){
+    return function(text){
+      splitText = text.split('\n');
+      formatedText = '<p>';
+      for(i = 0; i < splitText.length; i++){
+        formatedText += splitText[i] + '</p><p>';
+      }
+      formatedText += '</p>';
+      return formatedText;
+    }
+  })
+    
+  app.factory('Data', ['$http', function($http) {
+    var data = [];
+    var promise;
+
+    var req =  {
+      method: 'GET',
+      url: '/entries/api/entries/?format=json',
+    };
+
+    return {
+      getData: function(){
+        if(!promise){
+          
+          promise = $http(req).then(
+            function(resp){
+              data = resp.data;
+              /*
+              console.log(data.length);
+              console.log(data);
+              for(i = 0; i < data.length; i++){
+                console.log(i);
+                
+               // console.log(formatText(data[i].article));
+                //formatText(data[i].article);
+                //article = data[i].article;
+                //article = formatText(article);
+                //data[i].article = article;
+              }
+             */
+              return data;
+            }, 
+            function(resp){
+              return resp;
+            });
         };
-    });
+        
+        return promise;
+        },
+      getDetail: function(id) {
+        return data[id];
+      }
+    };
+  }]);
     
-    app.controller('DetailController', ['$scope', '$routeParams', 'Data', function($scope, $routeParams, Data) {
-        Data.getData().then(
-            function(data){
-                $scope.entry = data[$routeParams.entryID - 1];
-            },
-            function(error) {
-                console.log('error....');
-                console.log(error);
-            }
-        );
-        console.log($scope.entry);
-    }]);
+  app.controller('BlogController', ['$http','$scope', 'Data', function($http, $scope, Data){
     
+    function formatText(data) {
+    //console.log(text);
+      for(i = 0; i < data.length; i++){
+        console.log(i);
+        //console.log(data);
+        //text = data[i].article;
+        //console.log(text);
+        /*
+        splitText = text.split('\n');
+       
+        */
+        //data[i].article = formatedText;
+      }
+      
+      return data;
+    }
+    
+    Data.getData().then(
+      function(data){
+        $scope.articles = data;
+      },
+      function(resp){
+        console.log('error....');
+        console.log(resp);
+      });
+
+  }]);
+
+
+  app.directive('blogEntry', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/static/angular/entry.html',
+      controller: 'BlogController',
+      controllerAs: 'BlogCtrl'
+    };
+  });
+    
+  app.controller('DetailController', ['$scope', '$routeParams', 'Data', function($scope, $routeParams, Data) {
+    Data.getData().then(
+      function(data){
+        $scope.entry = data[$routeParams.entryID - 1];
+      },
+      function(error) {
+        console.log('error....');
+        console.log(error);
+      }
+    );
+  }]);
+
 })();
